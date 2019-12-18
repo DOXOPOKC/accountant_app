fb307a8b33a420a43a2dde7617c86670b76e1721
Spiridenko Pavel <spiridenko_po@kuzro.ru>
2019-12-18 14:24:59 +0700

    Init parse functions

--- src/backend/models/orm/contragent.py
+++ src/backend/models/orm/contragent.py
@@ -1,13 +1,22 @@
-from sqlalchemy import Column, String, Date
+from enum import Enum
+
+from sqlalchemy import Column, String, Date, Integer
+from sqlalchemy.dialects.postgresql import ENUM
 
 from models.orm.base_model import Base
 
 
+class KlassType(Enum):
+    ur = 'Юридическое лицо'
+    fl = 'Физическое лицо'
+
+
 class Contragent(Base):
-    name = Column(String, index=True)
-    inn = Column(String)
-    ur_adress = Column(String)
-    fuct_adress = Column(String)
-    ogrn = Column(String)
-    date = Column(Date)
-    klass = Column(String)
+    name = Column(String(), index=True)
+    inn = Column(Integer())
+    fuct_adress = Column(String())
+    klass = Column(ENUM(KlassType, name='klass_enum'),
+                   nullable=False, default=KlassType.ur)
+    ur_adress = Column(String())
+    ogrn = Column(Integer(), nullable=True)
+    date = Column(Date(), nullable=True)
--- src/backend/routes/endpoints/contragents.py
+++ src/backend/routes/endpoints/contragents.py
@@ -1,11 +1,12 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile, File
 from sqlalchemy.orm import Session
 
 from models.orm.contragent import Contragent
 from models.data.contragent import ContragentGet, ContragentBase
 from utils.db import get_db
+# from utils.parsers.contragents_xlsx import #TODO
 
 router = APIRouter()
 
@@ -13,3 +14,9 @@ router = APIRouter()
 @router.get('/contragents', response_model=List[ContragentGet])
 def get_contragents(db: Session = Depends(get_db)):
     return db.query(Contragent).all()
+
+
+@router.post('/contragents')
+def upload_contragents(db: Session = Depends(get_db),
+                       xlsx_file: UploadFile = File()):
+    
