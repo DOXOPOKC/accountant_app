3c9c570ef60412c55372b3fa5b52984dcd6ca919
Spiridenko Pavel <neo6666666@gmail.com>
2020-01-16 15:28:31 +0700

    Hotfix. Add docstrings.

--- backend/blackbird/views.py
+++ backend/blackbird/views.py
@@ -6,12 +6,64 @@ import calendar
 
 
 def calculate(*args, **kwargs):
-    since_date = kwargs.get('since_date', None)  # Дата с начала расчета
-    up_to_date = kwargs.get('up_to_date', None)  # Дата конца расчета
-    stat_value = kwargs.get('stat_value', None)  # Значение показателя (м2, кол. чел. и т.д.)
-    norm_value = kwargs.get('norm_value', None)  # Ключ на норматив
+    """
+    Функция принимает на вход словарь с параметрами
+        since_date: Дата с начала расчета
+        up_to_date: Дата конца расчета
+        stat_value: Значение показателя (м2, кол. чел. и т.д.)
+        norm_value: Ключ на норматив
+
+    Если параметров нет (или переданы не все) вызывает AttributeError().
+
+    Функция итерирует по датам, берет формулу расчета в виде строки на конец
+    месяца, вычисляет некоторые необходимые в расчетах значения и возвращает
+    массив словарей.
+
+    Формулы:
+        formula_obj_r: Объект формулы грубого подсчета
+        formula_rough: Формула грубого подсчета(в виде строки)
+        formula_obj_p: Объект формулы точного подсчета
+        formula_precise: Формула точного подсчета(в виде строки)
+
+    Значения которые могут быть использованы в формулах:
+    -------------------------------------------------------------------------
+        curr_date: Текущая дата
+        norm: Норматив на текущую дату
+        m_day_count: Количество дней в месяце
+        a_day_count: Количество активных дней
+    -------------------------------------------------------------------------
+    Вычисляемые значения:
+        V_as_rough: Грубый объем расситаный по формуле
+        V_as_precise: Точный объем рассчитаный по формуле
+        summ_precise: Точная сумма по объему
+        tax_price_precise: Точный НДС по объему
+        summ_tax_precise: Точная сумма с НДС
+        summ_rough: Грубая сумма по объему
+        tax_price_rough: Грубый НДС по объему
+        summ_tax_rough: Грубая сумма с НДС
+        tariff_tax: Тариф с НДС
+    Словарь результата:
+        curr_date: Текущая дата
+        V_as_rough: Грубый объем расситаный по формуле
+        summ_rough: Грубая сумма по объему
+        tax_price_rough: Грубый НДС по объему
+        summ_tax_rough: Грубая сумма с НДС
+        tax: НДС на текущую дату
+        tariff: Тариф на текущую дату
+        tariff_tax: Тариф с НДС
+        V_as_precise: Точный объем рассчитаный по формуле
+        summ_precise: Точная сумма по объему
+        tax_price_precise: Точный НДС по объему
+        summ_tax_precise: Точная сумма с НДС
+    """
+    since_date = kwargs.get('since_date', None)
+    up_to_date = kwargs.get('up_to_date', None)
+    stat_value = kwargs.get('stat_value', None)
+    norm_value = kwargs.get('norm_value', None)
 
     # Что делать с активными днями и днями невывоза?
+    # Что еще добавить сюда?
+    # Переработать механизм вычисления по формуле?
 
     if not (stat_value and norm_value and since_date and up_to_date):
         raise AttributeError()
@@ -26,28 +78,30 @@ def calculate(*args, **kwargs):
                          month=dt[1],
                          year=dt[0])
         print(curr_date)
-        norm = get_normative(curr_date, norm_value)  # Норматив
+        norm = get_normative(curr_date, norm_value)
 
-        formula_obj_r = get_formula_object(curr_date)  # Объект формулы грубый подсчет
-        formula_rough = formula_obj_r.get_formula()  # Формула (в виде строки)
+        formula_obj_r = get_formula_object(curr_date)
+        formula_rough = formula_obj_r.get_formula()
 
-        formula_obj_p = get_formula_object(curr_date, rough=False) # Объект формулы точный расчет
-        formula_precise = formula_obj_p.get_formula()  # Формулы точного рассчета в вике строки
+        formula_obj_p = get_formula_object(curr_date, rough=False)
+        formula_precise = formula_obj_p.get_formula()
 
-        m_day_count = calendar.monthrange(curr_date.year, curr_date.month)[1]  # Количество дней в месяце
-        a_day_count = m_day_count - start_day  # Количество активных дней
-        V_as_rough = eval(eval(formula_rough))  # Грубый объем расситаный по формуле
-        V_as_precise = eval(eval(formula_precise))  # точный объем рассчитаный по формуле
+        m_day_count = calendar.monthrange(curr_date.year, curr_date.month)[1]
+        a_day_count = m_day_count - start_day
+        print(norm, a_day_count)
 
-        summ_precise = V_as_precise * formula_obj_p.get_tariff()  # Точная сумма по объему
-        tax_price_precise = summ_precise * formula_obj_p.get_tax()  # Точный НДС по объему
-        summ_tax_precise = summ_precise + tax_price_precise  # Точная сумма с НДС
+        V_as_rough = eval(eval(formula_rough))
+        V_as_precise = eval(eval(formula_precise))
 
-        summ_rough = V_as_rough * formula_obj_r.get_tariff()  # Грубая сумма по объему
-        tax_price_rough = summ_rough * formula_obj_r.get_tax()  # Грубый НДС по объему
-        summ_tax_rough = summ_rough + tax_price_rough  # Грубая сумма с НДС
+        summ_precise = V_as_precise * formula_obj_p.get_tariff()
+        tax_price_precise = summ_precise * formula_obj_p.get_tax()
+        summ_tax_precise = summ_precise + tax_price_precise
 
-        tariff_tax = formula_obj_p.get_tariff() * (1 + formula_obj_p.get_tax())  # Тариф с НДС
+        summ_rough = V_as_rough * formula_obj_r.get_tariff()
+        tax_price_rough = summ_rough * formula_obj_r.get_tax()
+        summ_tax_rough = summ_rough + tax_price_rough
+
+        tariff_tax = formula_obj_p.get_tariff() * (1 + formula_obj_p.get_tax())
 
         result.append(
             {
@@ -69,6 +123,9 @@ def calculate(*args, **kwargs):
 
 
 def get_formula_object(curr_date, rough=True):
+    """ Функция возвращает объект формулы на переданную дату. Параметр `rough`
+    отвечает за то, будет формула грубого подсчета или нет.
+    """
     formulas = Formula.objects.filter(is_rough=rough)
     for f in formulas:
         if f.since_date <= curr_date <= f.up_to_date:
@@ -76,6 +133,9 @@ def get_formula_object(curr_date, rough=True):
 
 
 def get_normative(curr_date, norm_value):
+    """ Функция возвращает значение норматива на переданную дату.
+    Параметр `pk` первичный ключ норматива.
+    """
     norm = NormativeCategory.objects.get(pk=norm_value)
     norm_vals = norm.normative.all()
     for n in norm_vals:
