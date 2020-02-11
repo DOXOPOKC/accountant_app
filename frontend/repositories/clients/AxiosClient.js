63f7fd96a217758397d7e049662a95ea4ae4569b
DOXOPOKC <lilondin@gmail.com>
2020-02-11 12:38:56 +0700

    Fix env

--- frontend/repositories/clients/AxiosClient.js
+++ frontend/repositories/clients/AxiosClient.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const protocol = process.env.NUXT_ENV_PROTOCOL
 const domain = process.env.NUXT_ENV_DOMAIN
 
-const baseDomain = `${protocol}://${domain}/`
+const baseDomain = 'http://jud-module.lf.ru'
 const baseURL = `${baseDomain}`
 
 export default axios.create({
