8bf76a247aeff8385959a2b9270e5590693b96e6
Spiridenko Pavel <neo6666666@gmail.com>
2020-02-12 17:34:55 +0700

    Add new filter. Fix others.

--- backend/bluebird/templatetags/template_extra_filters.py
+++ backend/bluebird/templatetags/template_extra_filters.py
@@ -11,9 +11,12 @@ register = template.Library()
 @register.filter(name='literal')
 @stringfilter
 def literal(value):
-    return decimal2text(decimal.Decimal(value),
-                        int_units=((u'рубль', u'рубля', u'рублей'), 'm'),
-                        exp_units=((u'копейка', u'копейки', u'копеек'), 'f'))
+    if bool(value) or value is not None:
+        return decimal2text(decimal.Decimal(value),
+                            int_units=((u'рубль', u'рубля', u'рублей'), 'm'),
+                            exp_units=((u'копейка', u'копейки', u'копеек'),
+                                       'f'))
+    return ''
 
 
 @register.filter(name='percentage')
@@ -26,38 +29,56 @@ def percentage(value, arg: int = 1):
 
 @register.filter(name='gent_case')
 def gent_case(value: str):
-    return gent_case_filter(value)
+    if bool(value) or value is not None:
+        return gent_case_filter(value)
+    return ''
 
 
 def gent_case_filter(value: str):
-    morph = pymorphy2.MorphAnalyzer()
-    normalizer_val = value.strip()
-    word_list = normalizer_val.split(' ')
-    res = list()
-    for word in word_list:
-        parsed_phrase = morph.parse(word)[0]
-        gent_form = parsed_phrase.inflect({'gent'})[0]
-        res.append(gent_form)
-    return str(' '.join(res)).title()
+    if bool(value) or value is not None:
+        morph = pymorphy2.MorphAnalyzer()
+        normalizer_val = value.strip()
+        word_list = normalizer_val.split(' ')
+        res = list()
+        for word in word_list:
+            parsed_phrase = morph.parse(word)[0]
+            gent_form = parsed_phrase.inflect({'gent'})[0]
+            res.append(gent_form)
+        return str(' '.join(res)).title()
+    return ''
 
 
 def pretty_date_filter(date_value):
-    months = ('января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля',
-              'августа', 'сентября', 'октября', 'ноября', 'декабря')
-    return f'"{date_value.day}" {months[date_value.month-1]} {date_value.year}'
+    if bool(date_value) or date_value is not None:
+        months = ('января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+                  'июля', 'августа', 'сентября', 'октября', 'ноября',
+                  'декабря')
+        return f'"{date_value.day}" {months[date_value.month-1]}\
+{date_value.year}'
+    return ''
 
 
 def datv_case_filter(value: str):
-    morph = pymorphy2.MorphAnalyzer()
-    normalizer_val = value.strip()
-    word_list = normalizer_val.split(' ')
-    res = list()
-    for word in word_list:
-        parsed_phrase = morph.parse(word)[0]
-        gent_form = parsed_phrase.inflect({'datv'})[0]
-        res.append(gent_form)
-    return str(' '.join(res)).title()
+    if bool(value) or value is not None:
+        morph = pymorphy2.MorphAnalyzer()
+        normalizer_val = value.strip()
+        word_list = normalizer_val.split(' ')
+        res = list()
+        for word in word_list:
+            parsed_phrase = morph.parse(word)[0]
+            gent_form = parsed_phrase.inflect({'datv'})[0]
+            res.append(gent_form)
+        return str(' '.join(res)).title()
+    return ''
 
 
 def cap_first(value: str):
-    return value.capitalize()
+    if bool(value) or value is not None:
+        return value.capitalize()
+    return ''
+
+
+def proper_date_filter(date_value):
+    if bool(date_value) or date_value is not None:
+        return f'{date_value.day}.{date_value.month}.{date_value.year}'
+    return ''
