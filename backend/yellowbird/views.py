b252315d1f51d7e5836d8d17698d45803cb6b4b6
Spiridenko Pavel <neo6666666@gmail.com>
2020-03-02 13:59:13 +0700

    Change urls. Fix bugs.

--- backend/yellowbird/views.py
+++ backend/yellowbird/views.py
@@ -1,10 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 
 from .serializers import UserShortSerializer
 
 
 class UserView(APIView):
     def get(self, request):
-        serializer = UserShortSerializer(request.user)
+        user = request.user or None
+        serializer = UserShortSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
