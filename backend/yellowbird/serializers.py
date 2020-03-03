ce4b600814b1e62ffee6e79f784db23ca18d34e5
Spiridenko Pavel <neo6666666@gmail.com>
2020-03-03 15:40:34 +0700

    Quick fix.

--- backend/yellowbird/serializers.py
+++ backend/yellowbird/serializers.py
@@ -5,5 +5,5 @@ from .models import User
 class UserShortSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        # fields = '__all__'
         exclude = ['password', ]
