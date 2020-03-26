137630dc055621ecd978a961a682b0a9483af6e0
Spiridenko Pavel <neo6666666@gmail.com>
2020-03-26 15:16:15 +0700

    Change templates

--- backend/birds/settings.py
+++ backend/birds/settings.py
@@ -82,7 +82,7 @@ ROOT_URLCONF = 'birds.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, ''), ],
+        'DIRS': [BASE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
