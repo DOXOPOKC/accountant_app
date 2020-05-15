c74b2a21bbec5cf9026f4b4fd8b1dcaf2ee75ee6
Neo6666666 <neo6666666@gmail.com>
2020-05-15 11:45:17 +0700

    small fix kol'ca doktora manhettena

--- backend/bluebird/views.py
+++ backend/bluebird/views.py
@@ -216,11 +216,11 @@ class PackageView(APIView):
 
         zip_buffer.seek(0)
 
-        data = {
-            'filename': f'{pack.name_uuid}.zip',
-            'file': binascii.b2a_uu(zip_buffer.getvalue()),
-        }
-        response = HttpResponse(zip_buffer.getvalue(),
+        # data = {
+        #     'filename': f'{pack.name_uuid}.zip',
+        #     'file': binascii.b2a_uu(zip_buffer.getvalue()),
+        # }
+        response = HttpResponse(zip_buffer,
                                 content_type='application/zip')
         response['Content-Disposition'] = f'attachment; \
             filename={pack.name_uuid}.zip'
